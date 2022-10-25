d4c44853d252ff8e3c6414dec26fdacb869da44f
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-10-25 14:05:58 +0200

    doe

--- main.py
+++ main.py
@@ -1,58 +1,31 @@
-# Install tflite_runtime package to evaluate the model.
-pip3 install https://dl.google.com/coral/python/tflite_runtime-2.1.0.post1-cp36-cp36m-linux_x86_64.whl
-
-# Now we do evaluation on the tflite model.
 import os
-import numpy as np
-from tflite_runtime.interpreter import Interpreter
-from tflite_runtime.interpreter import load_delegate
+import pathlib
+from pycoral.utils import edgetpu
+from pycoral.utils import dataset
+from pycoral.adapters import common
+from pycoral.adapters import classify
 from PIL import Image
-from PIL import ImageDraw
-%matplotlib inline
 
-# Creates tflite interpreter
-interpreter = Interpreter('efficientnet-edgetpu-L_quant_edgetpu.tflite')
-# This exact code can be used to run inference on the edgetpu by simply creating
-# the instantialize the interpreter with libedgetpu delegates:
-# interpreter = Interpreter(args.model, experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+# Specify the TensorFlow model, labels, and image
+script_dir = pathlib.Path(__file__).parent.absolute()
+model_file = os.path.join(script_dir, 'mobilenet_v2_1.0_224_quant_edgetpu.tflite')
+label_file = os.path.join(script_dir, 'imagenet_labels.txt')
+image_file = os.path.join(script_dir, 'parrot.jpg')
+
+# Initialize the TF interpreter
+interpreter = edgetpu.make_interpreter(model_file)
 interpreter.allocate_tensors()
-interpreter.invoke() # warmup
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-width = input_details[0]['shape'][2]
-height = input_details[0]['shape'][1]
 
-def run_inference(interpreter, image):
-  interpreter.set_tensor(input_details[0]['index'], image)
-  interpreter.invoke()
-  boxes = interpreter.get_tensor(output_details[0]['index'])[0]
-  classes = interpreter.get_tensor(output_details[1]['index'])[0]
-  scores = interpreter.get_tensor(output_details[2]['index'])[0]
-  # num_detections = interpreter.get_tensor(output_details[3]['index'])[0]
-  return boxes, classes, scores
+# Resize the image
+size = common.input_size(interpreter)
+image = Image.open(image_file).convert('RGB').resize(size, Image.ANTIALIAS)
 
-test_image_paths = [os.path.join('/content/test', 'image{}.jpg'.format(i)) for i in range(1, 6)]
-for image_path in test_image_paths:
-  print('Evaluating:', image_path)
-  image = Image.open(image_path)
-  image_width, image_height = image.size
-  draw = ImageDraw.Draw(image)
-  resized_image = image.resize((width, height))
-  np_image = np.asarray(resized_image)
-  input_tensor = np.expand_dims(np_image, axis=0)
-  # Run inference
-  boxes, classes, scores = run_inference(interpreter, input_tensor)
-  # Draw results on image
-  colors = {0:(128, 255, 102), 1:(102, 255, 255)}
-  labels = {0:'abyssian cat', 1:'american bulldog'}
-  for i in range(len(boxes)):
-    if scores[i] > .7:
-      ymin = int(max(1, (boxes[i][0] * image_height)))
-      xmin = int(max(1, (boxes[i][1] * image_width)))
-      ymax = int(min(image_height, (boxes[i][2] * image_height)))
-      xmax = int(min(image_width, (boxes[i][3] * image_width)))
-      draw.rectangle((xmin, ymin, xmax, ymax), width=7, outline=colors[int(classes[i])])
-      draw.rectangle((xmin, ymin, xmax, ymin-10), fill=colors[int(classes[i])])
-      text = labels[int(classes[i])] + ' ' + str(scores[i]*100) + '%'
-      draw.text((xmin+2, ymin-10), text, fill=(0,0,0), width=2)
-  display(image)
+# Run an inference
+common.set_input(interpreter, image)
+interpreter.invoke()
+classes = classify.get_classes(interpreter, top_k=1)
+
+# Print the result
+labels = dataset.read_label_file(label_file)
+for c in classes:
+  print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
